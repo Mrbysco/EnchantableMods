f28b8a5699ba2feb625f9b7eaae3789b47a20365
Mrbysco <Mrbysco@users.noreply.github.com>
2024-06-03 20:02:24 +0200

    Adjust Forcecraft compat

--- src/main/java/com/mrbysco/enchantablemods/EnchantableMods.java
+++ src/main/java/com/mrbysco/enchantablemods/EnchantableMods.java
@@ -5,8 +5,6 @@ import com.mrbysco.enchantableblocks.EnchantableBlocks;
 import com.mrbysco.enchantableblocks.util.BlockReplacement;
 import com.mrbysco.enchantablemods.client.ClientHandler;
 import com.mrbysco.enchantablemods.forcecraft.ForceCompat;
-import com.mrbysco.enchantablemods.util.EnchantableModTags;
-import com.mrbysco.forcecraft.registry.ForceRegistry;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.InterModComms;
 import net.neoforged.fml.ModList;
@@ -39,29 +37,14 @@ public class EnchantableMods {
 	public void sendImc(InterModEnqueueEvent event) {
 		List<BlockReplacement> replacements = new ArrayList<>();
 		if (ModList.get().isLoaded("forcecraft")) {
-			replacements.add(new BlockReplacement(ForceRegistry.FORCE_FURNACE.get(), ForceCompat.ENCHANTED_FORCE_FURNACE, EnchantableModTags.FORCE_FURNACE_APPLICABLE));
-			replacements.add(new BlockReplacement(ForceRegistry.BLACK_FORCE_FURNACE.get(), ForceCompat.ENCHANTED_BLACK_FORCE_FURNACE, EnchantableModTags.FORCE_FURNACE_APPLICABLE));
-			replacements.add(new BlockReplacement(ForceRegistry.BLUE_FORCE_FURNACE.get(), ForceCompat.ENCHANTED_BLUE_FORCE_FURNACE, EnchantableModTags.FORCE_FURNACE_APPLICABLE));
-			replacements.add(new BlockReplacement(ForceRegistry.BROWN_FORCE_FURNACE.get(), ForceCompat.ENCHANTED_BROWN_FORCE_FURNACE, EnchantableModTags.FORCE_FURNACE_APPLICABLE));
-			replacements.add(new BlockReplacement(ForceRegistry.CYAN_FORCE_FURNACE.get(), ForceCompat.ENCHANTED_CYAN_FORCE_FURNACE, EnchantableModTags.FORCE_FURNACE_APPLICABLE));
-			replacements.add(new BlockReplacement(ForceRegistry.GRAY_FORCE_FURNACE.get(), ForceCompat.ENCHANTED_GRAY_FORCE_FURNACE, EnchantableModTags.FORCE_FURNACE_APPLICABLE));
-			replacements.add(new BlockReplacement(ForceRegistry.GREEN_FORCE_FURNACE.get(), ForceCompat.ENCHANTED_GREEN_FORCE_FURNACE, EnchantableModTags.FORCE_FURNACE_APPLICABLE));
-			replacements.add(new BlockReplacement(ForceRegistry.LIGHT_BLUE_FORCE_FURNACE.get(), ForceCompat.ENCHANTED_LIGHT_BLUE_FORCE_FURNACE, EnchantableModTags.FORCE_FURNACE_APPLICABLE));
-			replacements.add(new BlockReplacement(ForceRegistry.LIGHT_GRAY_FORCE_FURNACE.get(), ForceCompat.ENCHANTED_LIGHT_GRAY_FORCE_FURNACE, EnchantableModTags.FORCE_FURNACE_APPLICABLE));
-			replacements.add(new BlockReplacement(ForceRegistry.LIME_FORCE_FURNACE.get(), ForceCompat.ENCHANTED_LIME_FORCE_FURNACE, EnchantableModTags.FORCE_FURNACE_APPLICABLE));
-			replacements.add(new BlockReplacement(ForceRegistry.MAGENTA_FORCE_FURNACE.get(), ForceCompat.ENCHANTED_MAGENTA_FORCE_FURNACE, EnchantableModTags.FORCE_FURNACE_APPLICABLE));
-			replacements.add(new BlockReplacement(ForceRegistry.ORANGE_FORCE_FURNACE.get(), ForceCompat.ENCHANTED_ORANGE_FORCE_FURNACE, EnchantableModTags.FORCE_FURNACE_APPLICABLE));
-			replacements.add(new BlockReplacement(ForceRegistry.PINK_FORCE_FURNACE.get(), ForceCompat.ENCHANTED_PINK_FORCE_FURNACE, EnchantableModTags.FORCE_FURNACE_APPLICABLE));
-			replacements.add(new BlockReplacement(ForceRegistry.PURPLE_FORCE_FURNACE.get(), ForceCompat.ENCHANTED_PURPLE_FORCE_FURNACE, EnchantableModTags.FORCE_FURNACE_APPLICABLE));
-			replacements.add(new BlockReplacement(ForceRegistry.RED_FORCE_FURNACE.get(), ForceCompat.ENCHANTED_RED_FORCE_FURNACE, EnchantableModTags.FORCE_FURNACE_APPLICABLE));
-			replacements.add(new BlockReplacement(ForceRegistry.WHITE_FORCE_FURNACE.get(), ForceCompat.ENCHANTED_WHITE_FORCE_FURNACE, EnchantableModTags.FORCE_FURNACE_APPLICABLE));
+			ForceCompat.populateReplacements(replacements);
 		}
 		if (replacements.isEmpty()) {
 			LOGGER.warn("No replacements found. You don't have any compatible mods installed.");
-			return;
-		}
-		for (BlockReplacement replacement : replacements) {
-			InterModComms.sendTo(EnchantableBlocks.MOD_ID, "register_replacement", () -> replacement);
+		} else {
+			for (BlockReplacement replacement : replacements) {
+				InterModComms.sendTo(EnchantableBlocks.MOD_ID, "register_replacement", () -> replacement);
+			}
 		}
 	}
 }
